9438c3db3967d52e46bd1d7e7edd459dee2f2410
Kayode Owolabi Jesuseun <63752160+jesuseunkayode@users.noreply.github.com>
2022-05-10 00:35:35 +0100

    Add files via upload

---
+++ js/mytable.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $("#text").click(function(){
+    $("#cool").slideToggle("slow");
+  });
+
+  $("#text2").click(function(){
+    $("#cool2").slideToggle("slow");
+  });
+
+  $("#text3").click(function(){
+    $("#cool3").slideToggle("slow");
+  });
+
+  $("#text4").click(function(){
+    $("#cool4").slideToggle("slow");
+  });
+
+  $("#text5").click(function(){
+    $("#cool5").slideToggle("slow");
+  });
+
+  $("#text6").click(function(){
+    $("#cool6").slideToggle("slow");
+  });
+
+  $("#text7").click(function(){
+    $("#cool7").slideToggle("slow");
+  });
+
+  $("#text8").click(function(){
+    $("#cool8").slideToggle("slow");
+  });
+
+});
